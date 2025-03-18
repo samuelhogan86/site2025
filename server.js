6254db671aa3da58cae3162c158881ebfd3cfd73
samuelhogan86 <samuelhogan86@gmail.com>
2025-03-18 17:02:38 +0000

    site bones looking realer

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-res.send("Hello from Express!");
-});
+app.use(express.static("public"));
 
 app.listen(3000, "0.0.0.0", () => {
 console.log("Server running on port 3000");
